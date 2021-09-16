d80a73e55b99914a9b58b64216ddea935d80d647
Lewis Ward <lewisjohnward+git1@gmail.com>
2021-09-16 20:02:48 +0100

    first day done gradient

---
+++ src/days/Day1.js
@@ -0,0 +1,48 @@
+import "./Day1.css";
+
+export const Day1 = () => {
+  const string = `.icon { 
+      padding: 50px;    
+      border-radius: 10px; 
+
+      font-family: Arial, Helvetica, sans-serif;   
+      text-align: center;    
+      font-weight: 900;    
+      color: white;
+      text-shadow: 5px 3px 10px black;   
+
+      background: rgb(2,0,36);   
+      background: linear-gradient(60deg, rgb(16, 12, 85) 0%, rgb(25, 103, 167) 25%, rgb(125, 226, 247) 99%);
+  }
+
+      .number {
+        font-size: 6rem;
+        line-height: 60px;
+      }
+      
+      .days {
+        font-size: 3.5rem;
+      }
+      
+      .challenge {
+        font-size: 1rem;
+      }
+  `;
+  return (
+    <div className="day-container">
+      <p className="day-text">1</p>
+      <div className="container">
+        <div>
+          <div className="icon">
+            <p className="number">100</p>
+            <p className="days">DAYS</p>
+            <p className="challenge">CSS CHALLENGE</p>
+          </div>
+        </div>
+        <pre>
+          <code>{string}</code>
+        </pre>
+      </div>
+    </div>
+  );
+};
