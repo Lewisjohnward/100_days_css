a8d67edc3cbc1bfb6388d61a61a018e0581885c9
Lewis Ward <lewisjohnward+git1@gmail.com>
2021-09-16 19:59:28 +0100

    Updates

--- static/js/main.2bc2103b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp100_days_css=this.webpackJsonp100_days_css||[]).push([[0],{9:function(e,s,t){"use strict";t.r(s);var c=t(1),n=t.n(c),r=t(3),d=t.n(r),i=t(0);var o=function(){return Object(i.jsx)("div",{children:Object(i.jsx)("p",{children:"Hello"})})};d.a.render(Object(i.jsx)(n.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.2bc2103b.chunk.js.map
